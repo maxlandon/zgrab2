db9ff8cefcd22ab89ef0aec9524a189eb2f5c750
maxlandon <maximelandon@gmail.com>
2022-04-14 15:50:28 -0400

    New entrypoint to zgrab2 binary (zgrab2/cmd/zgrab2)
    This commit only includes the new main CLI file. Its very small size
    however should come with associated explanations:
    
    **Overview**
    
    - The new zgrab2 makes use of cobra as a hidden CLI library. Meaning
      that cobra-related code is almost completely absent from zgrab2 code.
      This is done through github.com/maxlandon/sflags/gen/gcobra struct
      parser, a replacement for jessevdk/go-flags.
    - The file includes an automatic completion generator, which on the long
      run, will allow for a much better and more portable CLI usage of
      zgrab2. Developement time will benefit from this as well.
    
    **Constraints**
    - To enable compiling zgrab2 functionality either as a purely local
      component or as a client-server remote one, while maintaining the
      possibility of installing the tool with a simple `go install
      github.com/zmap/zgrab2/cmd/zgrab2@latest`, we needed to declare
      separate subpackages in the bin/ directory. These are added and
      described in the next commit.
    
    Finally, the binary includes a call to the bin/local subpackage root
    cobra command, itself generated from our root zgrab2 config flags
    struct. Explained in the next commit.

--- cmd/zgrab2/main.go
+++ cmd/zgrab2/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	"github.com/zmap/zgrab2/bin"
-	_ "github.com/zmap/zgrab2/modules"
+	"log"
+
+	// Automatic completion generation.
+	"github.com/octago/sflags/gen/gcomp"
+
+	// Import paths of zgrab2 components:.
+	// bin/local    => everything in one binary, with CLI.
+	// bin/client   => only the modules CLI interface.
+	// bin/server   => only the scanners, to be served remotely.
+	"github.com/zmap/zgrab2/bin/local"
+
+	"github.com/zmap/zgrab2/module"
 )
 
-// main wraps the "true" main, bin.ZGrab2Main(), after importing all scan
-// modules in ZGrab2.
+// main is the entrypoint of a local scanner binary:
+// It must parse the command line onto our modules in the
+// client package, then actually run their associated scanners
+// in the server package.
 func main() {
-	bin.ZGrab2Main()
+	// Generate the completions for the entire zgrab2 application.
+	gcomp.Generate(local.Zgrab, &module.Config{}, nil)
+
+	// The root command will roughly follow these steps:
+	// 1 - simply parses the command-line flags (no exec)
+	// 2 - finds the command invoked (eg. ssh/http/multiple)
+	// 3 - passes the struct flags to the corresponding scanner.
+	// 4 - Runs the scanners that we have set up in 3).
+	if err := local.Zgrab.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
