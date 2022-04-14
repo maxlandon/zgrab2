887e714b6d6924509c5c5087e1d0484c334a1870
maxlandon <maximelandon@gmail.com>
2022-04-14 17:28:55 -0400

    Intermediary commit: remove all old module code.

--- modules/flags/bacnet.go
+++
@@ -1,9 +0,0 @@
-package flags
-
-// Bacnet holds the command-line configuration for the bacnet scan module.
-// Populated by the framework.
-type Bacnet struct {
-	BaseFlags `group:"base"`
-	UDPFlags  `group:"udp"`
-	Verbose   bool `long:"verbose" description:"More verbose logging, include debug fields in the scan results"`
-}
